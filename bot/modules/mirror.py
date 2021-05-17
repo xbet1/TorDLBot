b20b44d2dc64c314b37a67a2bbb061eccd961860
NiTRONDC1 <63654323+NiTRONDC1@users.noreply.github.com>
2021-05-17 21:10:08 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -152,9 +152,9 @@ class MirrorListener(listeners.MirrorListeners):
             buttons = button_build.ButtonMaker()
             if SHORTENER is not None and SHORTENER_API is not None:
                 surl = requests.get('https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, link)).text
-                buttons.buildbutton("☁️Drive Link☁️", surl)
+                buttons.buildbutton("🔰Drive Link🔰", surl)
             else:
-                buttons.buildbutton("☁️Drive Link☁️", link)
+                buttons.buildbutton("🔰Drive Link🔰", link)
             LOGGER.info(f'Done Uploading {download_dict[self.uid].name()}')
             if INDEX_URL is not None:
                 url_path = requests.utils.quote(f'{download_dict[self.uid].name()}')
@@ -163,9 +163,9 @@ class MirrorListener(listeners.MirrorListeners):
                     share_url += '/'
                 if SHORTENER is not None and SHORTENER_API is not None:
                     siurl = requests.get('https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, share_url)).text
-                    buttons.buildbutton("⚡Index Link⚡", siurl)
+                    buttons.buildbutton("🏓Index Link🏓", siurl)
                 else:
-                    buttons.buildbutton("⚡Index Link⚡", share_url)
+                    buttons.buildbutton("🏓Index Link🏓", share_url)
             if BUTTON_THREE_NAME is not None and BUTTON_THREE_URL is not None:
                 buttons.buildbutton(f"{BUTTON_THREE_NAME}", f"{BUTTON_THREE_URL}")
             if BUTTON_FOUR_NAME is not None and BUTTON_FOUR_URL is not None:
