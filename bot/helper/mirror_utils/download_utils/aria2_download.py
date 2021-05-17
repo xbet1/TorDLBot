5848118600f5901e8f6e5acfabf36530742651fd
NiTRONDC1 <63654323+NiTRONDC1@users.noreply.github.com>
2021-05-17 20:25:37 +0530

    Update aria2_download.py

--- bot/helper/mirror_utils/download_utils/aria2_download.py
+++ bot/helper/mirror_utils/download_utils/aria2_download.py
@@ -31,8 +31,8 @@ class AriaDownloadHelper(DownloadHelper):
             gdrive = GoogleDriveHelper(None)
             smsg, button = gdrive.drive_list(sname)
           if smsg:
-              dl.getListener().onDownloadError(f'File is already available in Drive.\n\n')
-              sendMarkup("Here are the search results:", dl.getListener().bot, dl.getListener().update, button)
+              dl.getListener().onDownloadError(f'🤬File is already available in Drive🤬.\n\n')
+              sendMarkup("🥶Here are the search results:👇", dl.getListener().bot, dl.getListener().update, button)
               aria2.remove([download])
               return
 
@@ -66,13 +66,13 @@ class AriaDownloadHelper(DownloadHelper):
     def __onDownloadPause(self, api, gid):
         LOGGER.info(f"onDownloadPause: {gid}")
         dl = getDownloadByGid(gid)
-        dl.getListener().onDownloadError('Download stopped by user!')
+        dl.getListener().onDownloadError('Download stopped by user❗️🥺')
 
     @new_thread
     def __onDownloadStopped(self, api, gid):
         LOGGER.info(f"onDownloadStop: {gid}")
         dl = getDownloadByGid(gid)
-        if dl: dl.getListener().onDownloadError('Dead torrent!')
+        if dl: dl.getListener().onDownloadError('Dead torrent❗️🤣')
 
     @new_thread
     def __onDownloadError(self, api, gid):
