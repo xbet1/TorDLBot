4a13e7ae09ccce6ef26f2349e9e88621018ab722
xbet1 <84500166+xbet1@users.noreply.github.com>
2021-05-20 20:57:56 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -133,7 +133,7 @@ class MirrorListener(listeners.MirrorListeners):
             uname = f"@{self.message.from_user.username}"
         else:
             uname = f'<a href="tg://user?id={self.message.from_user.id}">{self.message.from_user.first_name}</a>'
-        msg = f"{uname} your download has been stopped due to: {error}"
+        msg = f"{uname} 𝚢𝚘𝚞𝚛 𝚍𝚘𝚠𝚗𝚕𝚘𝚊𝚍 𝚑𝚊𝚜 𝚋𝚎𝚎𝚗 𝚜𝚝𝚘𝚙𝚙𝚎𝚍🤥 𝚍𝚞𝚎 𝚝𝚘: {error}"
         sendMessage(msg, self.bot, self.update)
         if count == 0:
             self.clean()
